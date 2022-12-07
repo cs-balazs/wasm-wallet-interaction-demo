a5b26e963d628b7e20629b667af850cfaea4a6b3
Csete Balázs <csetebalu@gmail.com>
2022-12-07 09:52:30 +0100

    next config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
-/** @type {import('next').NextConfig} */
+/** @type {import("next").NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   webpack(config) {
     config.experiments = {
+      layers: true,
       asyncWebAssembly: true,
     }
 
