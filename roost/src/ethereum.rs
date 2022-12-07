dd50522ce85f6a959c76a1b2f7ec6f0409a6c8a4
Csete Balázs <csetebalu@gmail.com>
2022-12-07 11:26:12 +0100

    somewhat less spaghetti ethereum signing

--- roost/src/ethereum.rs
+++ roost/src/ethereum.rs
@@ -1,54 +1,49 @@
-use js_sys::{Function, JsString, Promise};
+use js_sys::{Error, Function, JsString, Promise};
 use wasm_bindgen::{prelude::wasm_bindgen, JsValue};
 use web_sys::{console, window};
 
-fn get_sign_request(msg: &str, address: String) -> js_sys::Object {
+fn get_sign_request(msg: &str, address: String) -> Result<js_sys::Object, Error> {
     let sign_request_param = js_sys::Object::new();
     let sign_params = js_sys::Array::new();
     js_sys::Reflect::set(
         &sign_request_param,
         &"method".into(),
         &"personal_sign".into(),
-    )
-    .unwrap();
+    )?;
     js_sys::Array::push(&sign_params, &msg.into());
     js_sys::Array::push(&sign_params, &address.clone().into());
-    js_sys::Reflect::set(&sign_request_param, &"params".into(), &sign_params).unwrap();
-    sign_request_param
+    js_sys::Reflect::set(&sign_request_param, &"params".into(), &sign_params)?;
+    Ok(sign_request_param)
 }
 
 #[wasm_bindgen]
-pub async fn ethereum_sign(message: &str) {
+pub async fn ethereum_sign(message: &str) -> Result<(), Error> {
     let window = window().expect("Failed to access window object");
 
-    let ethereum: JsValue = window
+    let ethereum = window
         .get("ethereum")
-        .expect("Failed to access window.ethereum")
-        .try_into()
         .expect("Failed to access window.ethereum");
 
     console::log_1(&ethereum);
 
-    let address: String = js_sys::Reflect::get(&ethereum, &JsString::from("selectedAddress"))
-        .expect("Failed to access window.ethereum.selectedAddress")
+    let address: String = js_sys::Reflect::get(&ethereum, &JsString::from("selectedAddress"))?
         .as_string()
-        .expect("Failed to access window.ethereum.selectedAddress");
+        .expect("Failed to convert window.ethereum.selectedAddress to String");
 
     console::log_1(&format!("Hello {}! Please sign this.", &address).into());
 
-    let request: Function = js_sys::Reflect::get(&ethereum, &JsString::from("request"))
-        .unwrap()
-        .into();
+    let request: Function = js_sys::Reflect::get(&ethereum, &JsString::from("request"))?.into();
 
-    let sign_request = get_sign_request(message, address);
+    let sign_request = get_sign_request(message, address)?;
 
-    let res: Promise = request.call1(&JsValue::NULL, &sign_request).unwrap().into();
+    let res: Promise = request.call1(&JsValue::NULL, &sign_request)?.into();
 
     let signature: String = wasm_bindgen_futures::JsFuture::from(res)
-        .await
-        .unwrap()
+        .await?
         .as_string()
-        .unwrap();
+        .expect("Failed to convert signature to String.");
 
     console::log_1(&format!("Signature: {}", &signature).into());
+
+    Ok(())
 }
